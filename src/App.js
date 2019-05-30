5d7aeadcad9e64709bc67b8686a96d01e7f1d947
kwesiatherley <kwesiatherley@gmail.com>
2019-05-30 17:19:52 -0400

    Update onChange with correct spelling

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChnage={updateSearch} />
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">
           Search
         </button>
