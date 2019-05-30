11a9182292ebd3eb677a74278b87ac0203143c75
kwesiatherley <kwesiatherley@gmail.com>
2019-05-30 17:03:02 -0400

    Updates

---
+++ precache-manifest.598d0107b8910074f4fec617f5dc5ea7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3962d06bc1928df8b923837d321a2a",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "12920052b9f9f2c75518",
+    "url": "/recipe-app/static/css/main.c2fe6d07.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/recipe-app/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "12920052b9f9f2c75518",
+    "url": "/recipe-app/static/js/main.041971fa.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  }
+]);
